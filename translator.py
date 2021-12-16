283d3eb9da35c738bc05726a0b5d1b576ada9d4c
shi-anli <suloversanja@gmail.com>
2021-12-16 04:09:44 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,34 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
 
-LANGUAGES = {"1": "Arabic", "2": "German", "3": "English", "4": "Spanish", "5": "French", "6": "Hebrew", "7": "Japanese", "8": "Dutch", "9": "Polish", "10": "Portuguese", "11": "Romanian", "12": "Russian", "13": "Turkish"}
+LANGUAGES = ["0", "Arabic", "German", "English", "Spanish", "French", "Hebrew", "Japanese", "Dutch", "Polish", "Portuguese", "Romanian", "Russian", "Turkish"]
 
 
 def welcome():
-    print("List of supported languages:")
-    print([f"{key}. {value}" for key, value in LANGUAGES.items()], sep="\n")
-    source_lang = LANGUAGES[input("number of source language: ")].lower()
-    target_lang = LANGUAGES[input("number of target language: ")].lower()
-    word = input("the word to translate: ")
+    print("Hello, you're welcome to the translator. Translator supports:")
+    print(*[f"{LANGUAGES.index(l)}. {l}" for l in LANGUAGES[1:]], sep="\n")
+    source_lang = LANGUAGES[int(input("Type the number of your language: "))].lower()
+    target_lang = LANGUAGES[int(input("Type the number of a language you want to translate to or '0' to translate to all languages: "))].lower()
+    word = input("Type the word you want to translate: ")
     main(target_lang, source_lang, word)
 
 
 def word_translations(soup):
-    words = soup.find_all("a", class_="translation")
-    return [w.text.strip() for w in words]
+    # words = soup.find_all("a", class_="translation")
+    # return [w.text.strip() for w in words]
+    word = (soup.find_all("a", class_="translation"))[1].text.strip()
+    return word
 
 
 def sent_translations(soup):
-    sentences = soup.find_all("div", class_=("src ltr", "trg ltr"))
-    return [s.text.strip() for s in sentences]
+    # sentences = soup.find_all("div", class_=("src ltr", "trg ltr"))
+    # return [s.text.strip() for s in sentences]
+    src_sentence = soup.find("div", class_="src").text.strip()
+    trg_sentence = soup.find("div", class_="trg").text.strip()
+    return src_sentence, trg_sentence
 
 
 def main(target_lang, source_lang, word):
-    url = "https://context.reverso.net/translation/{}-{}/{}".format(source_lang, target_lang, word)
+    if target_lang == "0":
+        for k in LANGUAGES[1:]:
+            if k.lower() != source_lang:
+                main(k.lower(), source_lang, word)
+        return
+    else:
+        url = "https://context.reverso.net/translation/{}-{}/{}".format(source_lang, target_lang, word)
     r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     soup = BeautifulSoup(r.content, "html.parser")
-    print(f"{target_lang.capitalize()} Translations", *word_translations(soup), sep="\n", end="\n \n")
-    print(f"{target_lang.capitalize()} Examples", *sent_translations(soup), sep="\n")
+    target_lang = target_lang.capitalize()
+    output = [f"{target_lang} Translations:\n",
+              word_translations(soup),
+              "\n\n",
+              f"{target_lang} Example:\n",
+              "\n".join(sent_translations(soup)),
+              "\n\n"]
+    output = "".join(output)
+    save(word, output)
+    print(*open(f"{word}.txt", "r").readlines(), sep="")
+
+
+def save(word, output):
+    with open(f"{word}.txt", "a", encoding="utf-8") as file:
+        file.write(output)
 
 
 welcome()
